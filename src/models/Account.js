d2298225c715da105a4708a273719b6827da29db
ctn2002 <61001185+ctn2002@users.noreply.github.com>
2022-08-06 08:46:04 +0700

    Update Account.js

--- src/models/Account.js
+++ src/models/Account.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  email: { type: String, maxLength: 100 },
-  password: { type: String, maxLength: 100 },
+  email: { type: String, maxLength: 256 },
+  password: { type: String, maxLength: 16 },
+  lastLogin: { type: Date },
+  status: { type: String, maxLength: 15 },
   users: [{ type: Schema.Types.ObjectId, ref: "Users" }],
 });
 
