11573d5a5a4f62a274ff927d0c9b8066e9227603
ctn2002 <61001185+ctn2002@users.noreply.github.com>
2022-08-06 08:52:32 +0700

    Update Author.js

--- src/models/Author.js
+++ src/models/Author.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const authorSchema = new Schema({
-  name: { type: String, maxLength: 100 },
+  name: { type: String, maxLength: 40 },
   birth: { type: Date },
   gender: { type: String, maxLength: 5 },
-  nationality: { type: String, maxLength: 20 },
+  nationality: { type: String, maxLength: 32 },
 });
 
 module.exports = mongoose.model("Authors", authorSchema);
