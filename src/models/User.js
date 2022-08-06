aae168b411c417568a2e2836b0690f3bbb185142
ctn2002 <61001185+ctn2002@users.noreply.github.com>
2022-08-06 08:43:30 +0700

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  name: { type: String, maxLength: 100 },
-  address: { type: String, maxLength: 100 },
+  name: { type: String, maxLength: 40 },
+  address: { type: String, maxLength: 120 },
   phone: { type: String, maxLength: 15 },
   birth: { type: Date },
   gender: { type: String, maxLength: 5 },
