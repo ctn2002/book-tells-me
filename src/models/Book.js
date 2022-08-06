648b68e43cc4700636396a1d1d0d6e9f221f4de9
ctn2002 <61001185+ctn2002@users.noreply.github.com>
2022-08-06 08:56:40 +0700

    Update Book.js

--- src/models/Book.js
+++ src/models/Book.js
@@ -3,13 +3,13 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const bookSchema = new Schema({
-  nameBook: { type: String, maxLength: 100 },
+  nameBook: { type: String, maxLength: 200 },
   description: { type: String, maxLength: 600 },
   image: { type: String, maxLength: 255 },
   urlDownload: { type: String, maxLength: 255 },
   // admins: { type: Schema.Types.ObjectId, ref: "Admins" },
-  genres: { type: String, maxLength: 100 },
-  author: { type: String, maxLength: 100 },
+  genres: { type: String, maxLength: 50 },
+  author: { type: String, maxLength: 40 },
 });
 
 module.exports = mongoose.model("Books", bookSchema);
